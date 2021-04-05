989bf9d9fb8b313a4ea3a62462a650a152b24571
almoskvin <sil.moskvin@gmail.com>
2021-04-05 21:48:29 +0100

    8.20 Implement the Checkout Application using parallel Streams

---
+++ src/main/java/com/learnjava/service/CheckoutService.java
@@ -0,0 +1,39 @@
+package com.learnjava.service;
+
+import com.learnjava.domain.checkout.Cart;
+import com.learnjava.domain.checkout.CartItem;
+import com.learnjava.domain.checkout.CheckoutResponse;
+import com.learnjava.domain.checkout.CheckoutStatus;
+import com.learnjava.util.CommonUtil;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CheckoutService {
+
+    private PriceValidatorService priceValidatorService;
+
+    public CheckoutService(PriceValidatorService priceValidatorService) {
+        this.priceValidatorService = priceValidatorService;
+    }
+
+    public CheckoutResponse checkout(Cart cart) {
+        CommonUtil.startTimer();
+        List<CartItem> expiredItems = cart.getCartItemList()
+                .stream()
+                .map(this::updateExpired)
+                .filter(CartItem::isExpired)
+                .collect(Collectors.toList());
+        CommonUtil.timeTaken();
+
+        if (expiredItems.size() > 0) {
+            return new CheckoutResponse(CheckoutStatus.FAILURE, expiredItems);
+        }
+        return new CheckoutResponse(CheckoutStatus.SUCCESS);
+    }
+
+    private CartItem updateExpired(CartItem cartItem) {
+        cartItem.setExpired(priceValidatorService.isCartItemInvalid(cartItem));
+        return cartItem;
+    }
+}
