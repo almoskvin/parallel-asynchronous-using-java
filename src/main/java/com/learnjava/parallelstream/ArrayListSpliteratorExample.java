17ec3b3aa64c0fadb838cc9334658401123eee8a
almoskvin <sil.moskvin@gmail.com>
2021-04-10 12:19:21 +0100

    8.24 Comparing Spliterator Performances - ArrayList vs LinkedList - Part 2
    quick note - parallelStream execution uses the ForkJoin framework.
    ArrayList performs better in parallel streams than LinkedList due to the spliterating cost of the latter.

--- src/main/java/com/learnjava/parallelstream/ArrayListSpliteratorExample.java
+++ src/main/java/com/learnjava/parallelstream/ArrayListSpliteratorExample.java
@@ -2,16 +2,15 @@ package com.learnjava.parallelstream;
 
 import com.learnjava.util.CommonUtil;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ArrayListSpliteratorExample {
-    public List<Integer> multiplyEachValue(ArrayList<Integer> inputList, int multiplyValue, boolean runParallel) {
+    public List<Integer> multiplyEachValue(List<Integer> inputList, int multiplyValue, boolean runParallel) {
         CommonUtil.stopWatchReset();
         CommonUtil.startTimer();
-        Stream<Integer> inputStream =runParallel ? inputList.parallelStream() : inputList.stream();
+        Stream<Integer> inputStream = runParallel ? inputList.parallelStream() : inputList.stream();
         List<Integer> resultList = inputStream
                 .map(v -> v * multiplyValue)
                 .collect(Collectors.toList());
--- src/test/java/com/learnjava/parallelstream/ArrayListSpliteratorExampleTest.java
+++ src/test/java/com/learnjava/parallelstream/ArrayListSpliteratorExampleTest.java
@@ -4,6 +4,7 @@ import com.learnjava.util.DataSet;
 import org.junit.jupiter.api.RepeatedTest;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,19 +14,37 @@ class ArrayListSpliteratorExampleTest {
     ArrayListSpliteratorExample example = new ArrayListSpliteratorExample();
 
     @RepeatedTest(5)
-    void multiplyEachValue() {
+    void multiplyEachValueArrayList() {
         int size = 1000000;
         ArrayList<Integer> testList = DataSet.generateArrayList(size);
-        System.out.println("Sequential");
+        System.out.println("AL Sequential");
         List<Integer> resultList = example.multiplyEachValue(testList, 2, false);
         assertEquals(size, resultList.size());
     }
 
     @RepeatedTest(5)
-    void multiplyEachValueInParallel() {
+    void multiplyEachValueArrayListInParallel() {
         int size = 1000000;
         ArrayList<Integer> testList = DataSet.generateArrayList(size);
-        System.out.println("Parallel");
+        System.out.println("AL Parallel");
+        List<Integer> resultList = example.multiplyEachValue(testList, 2, true);
+        assertEquals(size, resultList.size());
+    }
+
+    @RepeatedTest(5)
+    void multiplyEachValueLinkedList() {
+        int size = 1000000;
+        LinkedList<Integer> testList = DataSet.generateIntegerLinkedList(size);
+        System.out.println("LL Sequential");
+        List<Integer> resultList = example.multiplyEachValue(testList, 2, false);
+        assertEquals(size, resultList.size());
+    }
+
+    @RepeatedTest(5)
+    void multiplyEachValueLinkedListInParallel() {
+        int size = 1000000;
+        LinkedList<Integer> testList = DataSet.generateIntegerLinkedList(size);
+        System.out.println("LL Parallel");
         List<Integer> resultList = example.multiplyEachValue(testList, 2, true);
         assertEquals(size, resultList.size());
     }
